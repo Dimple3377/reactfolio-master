5bdc2803afd702fa4d39e3919a74fc773fa0301e
Polux Troy <joffran712@gmail.com>
2024-06-01 01:48:58 +0200

    Initial commit

---
+++ .history/src/data/user_20240425032659.js
@@ -0,0 +1,64 @@
+const INFO = {
+	main: {
+		title: "Portfolio by Dimple",
+		name: "Joffrey",
+		email: "joffran712@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Web integrator  and front-end developer.",
+		description:
+			"Passionate about web development, I am a web integrator specialized in the most sought-after technologies and languages, such as HTML, CSS, SASS, JavaScript, and the React & Redux frameworks. An expert in creating dynamic and responsive websites, I am proficient in API calls, responsive design, and development environments like Visual Studio Code. My expertise also includes solid version control skills with Git & GitHub, ensuring efficient collaboration and project management.",
+	},
+
+	about: {
+		title: "I’m Joffrey I live in Paris, where I design the future.",
+		description:
+			"Having recently completed my developer training, I've already engaged in various projects that I'm truly proud of. Many of these projects are open-source, offering an opportunity for others to explore, contribute, and enhance. If you're interested in my work, I encourage you to review the code and share any suggestions for improvement. I'm eager to collaborate and learn from feedback, as I believe it's a fantastic way to grow and refine my skills.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"A chronological compilation of my detailed reflections on programming, leadership, product design, and beyond.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"This project involved developing a mobile-first interface for a startup, emphasizing responsive web design from the outset. Using SASS for advanced CSS functionalities, the site features engaging CSS animations to enhance user interaction. The entire development process was managed through Git and GitHub, showcasing not only technical skills but also proficiency in project management and collaboration tools.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
---
+++ src/data/user.js
@@ -0,0 +1,64 @@
+const INFO = {
+	main: {
+		title: "Portfolio by Dimple",
+		name: "Joffrey",
+		email: "joffran712@gmail.com",
+		logo: "../logo.png",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Web integrator  and front-end developer.",
+		description:
+			"Passionate about web development, I am a web integrator specialized in the most sought-after technologies and languages, such as HTML, CSS, SASS, JavaScript, and the React & Redux frameworks. An expert in creating dynamic and responsive websites, I am proficient in API calls, responsive design, and development environments like Visual Studio Code. My expertise also includes solid version control skills with Git & GitHub, ensuring efficient collaboration and project management.",
+	},
+
+	about: {
+		title: "I’m Joffrey I live in Paris, where I design the future.",
+		description:
+			"Having recently completed my developer training, I've already engaged in various projects that I'm truly proud of. Many of these projects are open-source, offering an opportunity for others to explore, contribute, and enhance. If you're interested in my work, I encourage you to review the code and share any suggestions for improvement. I'm eager to collaborate and learn from feedback, as I believe it's a fantastic way to grow and refine my skills.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"A chronological compilation of my detailed reflections on programming, leadership, product design, and beyond.",
+	},
+
+	projects: [
+		{
+			title: "Enhance a Mobile Site Interface with CSS Animations",
+			description:
+				"This project involved developing a mobile-first interface for a startup, emphasizing responsive web design from the outset. Using SASS for advanced CSS functionalities, the site features engaging CSS animations to enhance user interaction. The entire development process was managed through Git and GitHub, showcasing not only technical skills but also proficiency in project management and collaboration tools.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://dimple3377.github.io/OC-ohmyfood-project-4/",
+		},
+		{
+			title: "Create a Real Estate Rental Web Application with React",
+			description:
+				"In this project, I implemented the front-end of an application using React and React Router to deliver a modern and responsive user experience.I focused on the presentation logic and React components, utilizing this popular JavaScript library to build user interfaces.React Router was used to manage navigation across various pages of the application, allowing me to effectively handle routing in a modern web application.The project was strictly front-end development, based on provided design mockups and utilizing simulated data from a JSON file.I started the project with Create React App, which simplifies the initial setup of a React application. Node.js was also employed to run JavaScript code outside the browser.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Dimple3377/kasa-project",
+		},
+		{
+			title: "Create a Dynamic Web Page with JavaScript",
+			description:
+				"This project involved creating a dynamic web page for the website of an interior designer using JavaScript and integrating with an API.The focus was on the front-end development, where I built a presentation page to showcase the designer's work using provided HTML. This required handling user events and manipulating DOM elements with JavaScript. Additionally, I developed a login page for the site administrator and a modal for uploading new media, all from scratch. This provided an excellent opportunity to deepen my JavaScript skills. For the first time, I had access to back-end code to test functionalities, learning how to communicate with an API—a key skill for modern web applications. The project utilized Figma for design prototypes, Visual Studio Code for development, and GitHub for version control.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com/Dimple3377/Portfolio-architecte-sophie-bluel-master",
+		},
+	],
+};
+
+export default INFO;
